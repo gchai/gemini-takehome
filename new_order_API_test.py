d9e5a48f47951397942e91de6f0c0a78323c68d6
Gabe C <g@bechai.com>
2021-09-21 05:43:45 -0400

    adding order options validation

--- new_order_API_test.py
+++ new_order_API_test.py
@@ -20,6 +20,7 @@ url = base_url + endpoint
 gemini_api_key = "account-X12nWyP3H5MwMqpO6yjf"
 gemini_api_secret = "9cvGrAbZCgvdZM4mvhpgK7V9nRK".encode()
 
+
 # Puts a 1 second delay between each test to ensure nonce tokens don't clash.
 # This can be alleviated for larger test sets by using multiple accounts
 def teardown_function(function):  # the function parameter is optional
@@ -34,6 +35,7 @@ with open("new_order_schema.json", "r") as file:
     order_status_schema = json.load(file)
 with open("error_schema.json", "r") as file:
     error_schema = json.load(file)
+
 
 # I used JSON Schema validation here to validate the JSON responses. At first I thought I should use RegEx
 # but that won't work if the order of the elements come back differently.
@@ -53,6 +55,7 @@ def validate_json(json_data, schema):
 
 
 # Abstracted this out since it's used a lot
+# This also assumes that the authentication works
 def call_api(payload):
     encoded_payload = json.dumps(payload).encode()
     b64 = base64.b64encode(encoded_payload)
@@ -141,10 +144,10 @@ def test_basic_happypath_tests(
     ), f'Order Type should be "{payload_type}", instead its {new_order["type"]}'
     assert float(new_order["original_amount"]) == float(
         payload_amount
-    ), f'Amount should be "{payload_amount}", instead its {new_order["type"]}'
+    ), f'Amount should be "{payload_amount}", instead its {new_order["original_amount"]}'
     assert float(new_order["price"]) == float(
         payload_price
-    ), f'Price should be "{payload_price}", instead its {new_order["type"]}'
+    ), f'Price should be "{payload_price}", instead its {new_order["price"]}'
 
 
 # I could have combined the negative and the happy path basic tests, but I like having the visual separation.
@@ -205,6 +208,7 @@ def test_basic_negative_tests(
     assert validate_json(new_order, error_schema)
 
 
+# Tests the stop limits and the variables it has
 @pytest.mark.parametrize(
     "test_name, payload_symbol, payload_amount, payload_price, payload_side, payload_type, payload_stop_price, schema, resp_code",
     [
@@ -242,6 +246,17 @@ def test_basic_negative_tests(
             "buy",
             "exchange stop limit",
             "4999.00",
+            order_status_schema,
+            200,
+        ),
+        (
+            "ETHBTC Buy Stop Limit Float",
+            "ethbtc",
+            ".1",
+            "2000.00",
+            "buy",
+            "exchange stop limit",
+            "1900.00",
             order_status_schema,
             200,
         ),
@@ -309,10 +324,30 @@ def test_stop_limit(
     new_order, status_code = call_api(payload)
     print(new_order)
 
-    assert (
-        status_code == resp_code
-    ), f"Status code should be {resp_code}, its {status_code}"
+    assert status_code == resp_code, f"Status code should be {resp_code}, its {status_code}"
     assert validate_json(new_order, schema)
+    if resp_code == 200:
+        assert (
+            new_order["symbol"] == payload_symbol
+        ), f'Symbol should be "{payload_symbol}", instead its {new_order["symbol"]}'
+        assert (
+            new_order["exchange"] == "gemini"
+        ), f'Exchange should be "gemini", instead its {new_order["exchange"]}'
+        assert (
+            new_order["side"] == payload_side
+        ), f'Side should be "{payload_side}", instead its {new_order["side"]}'
+        assert (
+            new_order["type"] == "stop-limit"
+        ), f'Order Type should be "stop-limit", instead its {new_order["type"]}'
+        assert float(new_order["original_amount"]) == float(
+            payload_amount
+        ), f'Amount should be "{payload_amount}", instead its {new_order["original_amount"]}'
+        assert float(new_order["price"]) == float(
+            payload_price
+        ), f'Price should be "{payload_price}", instead its {new_order["price"]}'
+        assert float(new_order["stop_price"]) == float(
+            payload_stop_price
+        ), f'Stop price should be "{payload_stop_price}", instead it is {new_order["stop_price"]}'
 
 
 # Tests that the client order ID returns the given ID.
@@ -366,3 +401,114 @@ def test_bad_nonce():
         new_order["reason"] == "InvalidNonce"
     ), f'Reason should be "InvalidNonce", instead its {new_order["reason"]}'
     assert validate_json(new_order, error_schema)
+
+
+# Tests the order options
+@pytest.mark.parametrize(
+    "test_name, payload_symbol, payload_amount, payload_price, payload_side, payload_type, payload_options, resp_code",
+    [
+        (
+            "Maker or Cancel Test",
+            "btcusd",
+            "5",
+            "3633.00",
+            "buy",
+            "exchange limit",
+            ["maker-or-cancel"],
+            200,
+        ),
+        (
+            "Immediate or Cancel Test",
+            "btcusd",
+            "5",
+            "3633.00",
+            "buy",
+            "exchange limit",
+            ["immediate-or-cancel"],
+            200,
+        ),
+        ("Fill or Kill", "btcusd", "5", "3633.00", "buy", "exchange limit", ["fill-or-kill"], 200),
+        # Auction tests can only run on certain times of the day.
+        # Couple ways we can handle this.
+        # Only run it during the hours that it's expected to run?
+        # Program in a function that checks the time and
+        #     if it's outside of normal hours we can expect a 400, otherwise a 200.
+        ("Auction Test", "btcusd", "5", "3633.00", "buy", "exchange limit", ["auction-only"], 200),
+        # Indication of interest runs into the same issue as before where btcusd has some bug that causes an invalid price error.
+        # Interestingly, the API response to multiple options seems to infer that "Indication if Interest" is no longer supported
+        # it states: A single order supports at most one of these options: ['maker-or-cancel', 'immediate-or-cancel', 'auction-only', 'fill-or-kill']
+        (
+            "Indication of Interest Test",
+            "ethbtc",
+            "5",
+            "1000.00",
+            "sell",
+            "exchange limit",
+            ["indication-of-interest"],
+            200,
+        ),
+        (
+            "Multiple Option Test",
+            "btcusd",
+            "5",
+            "3633.00",
+            "buy",
+            "exchange limit",
+            ["maker-or-cancel", "immediate-or-cancel"],
+            400,
+        ),
+    ],
+)
+def test_order_options(
+    test_name,
+    payload_symbol,
+    payload_amount,
+    payload_price,
+    payload_side,
+    payload_type,
+    payload_options,
+    resp_code,
+):
+    print(f"\n====== Test: {test_name} ======")
+    t = datetime.datetime.now()
+    payload_nonce = str(int(time.mktime(t.timetuple()) * 1000))
+
+    payload = {
+        "request": "/v1/order/new",
+        "nonce": payload_nonce,
+        "symbol": payload_symbol,
+        "amount": payload_amount,
+        "price": payload_price,
+        "side": payload_side,
+        "type": payload_type,
+        "options": payload_options,
+    }
+
+    new_order, status_code = call_api(payload)
+    print(new_order)
+
+    assert status_code == resp_code, f"Status code should be {resp_code}, its {status_code}"
+    assert validate_json(new_order, order_status_schema)
+
+    if resp_code == 200:
+        assert (
+            new_order["symbol"] == payload_symbol
+        ), f'Symbol should be "{payload_symbol}", instead its {new_order["symbol"]}'
+        assert (
+            new_order["exchange"] == "gemini"
+        ), f'Exchange should be "gemini", instead its {new_order["exchange"]}'
+        assert (
+            new_order["side"] == payload_side
+        ), f'Side should be "{payload_side}", instead its {new_order["side"]}'
+        assert (
+            new_order["type"] == payload_type
+        ), f'Order Type should be "{payload_type}", instead its {new_order["type"]}'
+        assert float(new_order["original_amount"]) == float(
+            payload_amount
+        ), f'Amount should be "{payload_amount}", instead its {new_order["original_amount"]}'
+        assert float(new_order["price"]) == float(
+            payload_price
+        ), f'Price should be "{payload_price}", instead its {new_order["price"]}'
+        assert (
+            new_order["options"] == payload_options
+        ), f'Order option should be {payload_options}, instead its {new_order["options"]}'
